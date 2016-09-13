8431af0aa62c558c30933256ee1ff4ba286220e2
cooper15 <v.centen15@gmail.com>
2016-09-12 22:49:38 -0600

    Upload secretary script.

---
+++ Secretary.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+from time import sleep
+import logging
+
+LS_COMMAND = "ls | grep "
+
+# Filter definitions
+# ---------------------------------------------------------------------------------------
+TUPLE_DOCUMENTS_FILTER = (".*.pdf.*", ".*.doc.*", ".*.docx.*", ".*.xls.*", ".*.xlsx.*")
+TUPLE_IMAGE_FILTERS = (".*jpg.*", ".*.png.*")
+# ---------------------------------------------------------------------------------------
+
+# Final paths
+# ---------------------------------------------------------------------------------------
+ORIGIN_PATH = "/home/cooper15/Descargas/"
+PICTURES_FINAL_PATH = " /home/cooper15/Imágenes/"
+COMPROBANTES_FINAL_PATH = " /home/cooper15/Dropbox/Comprobantes_de_Pago_electronico/"
+DOCUMENTS_FINAL_PATH =" /home/cooper15/Documentos"
+# ---------------------------------------------------------------------------------------
+
+def get_pictures():
+    pictures_name = []
+    for index in range(0, len(TUPLE_IMAGE_FILTERS)):
+        for line in os.popen(LS_COMMAND + TUPLE_IMAGE_FILTERS[index], 'r', 1):
+            pictures_name.append(line.strip())
+    return pictures_name
+
+
+def get_comprobantes():
+    documents_name = []
+    for line in os.popen(LS_COMMAND + "Comprobante", 'r', 1):
+        documents_name.append(line.strip())
+    return documents_name
+
+
+def get_documents():
+    documents_name = []
+    for index in range(0, len(TUPLE_IMAGE_FILTERS)):
+        for line in os.popen(LS_COMMAND + TUPLE_DOCUMENTS_FILTER[index], 'r', 1):
+            documents_name.append(line.strip())
+    return documents_name
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s %(message)s', filename="/home/cooper15/.scripts/secretary_mv.log", level=logging.DEBUG)
+    logging.debug("starting")
+    while True:
+        for picture in get_pictures():
+            os.system("mv " + "'" + picture + "'" + PICTURES_FINAL_PATH)
+            logging.debug("mv " + picture + " to " + PICTURES_FINAL_PATH)
+        for comprobante in get_comprobantes():
+            os.system("mv " + "'" + comprobante + "'" + COMPROBANTES_FINAL_PATH)
+            logging.debug("mv " + comprobante + " to " + COMPROBANTES_FINAL_PATH)
+        for document in get_documents():
+            os.system("mv " + "'" + document + "'" + DOCUMENTS_FINAL_PATH)
+            logging.debug("mv " + document + " to " + DOCUMENTS_FINAL_PATH)
+        sleep(30)
